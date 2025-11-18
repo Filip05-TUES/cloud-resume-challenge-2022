6d886211660de60733bf2edec3ac7dabfee05e31
Filip05-TUES <f.ermenkov@gmail.com>
2025-11-19 01:54:41 +0200

    Add view counter to website and update lambda code

--- bucket-content/website/pages/main/main.js
+++ bucket-content/website/pages/main/main.js
@@ -1,13 +1,30 @@
-var imdex = 0;
-var txtJob = 'IT Specialist';
-var speed = 100;
-
 document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(function writeJob() {
-        if (imdex < txtJob.length) {
-            document.getElementById("job").innerHTML += txtJob.charAt(imdex);
-            imdex++;
-            setTimeout(writeJob, speed);
-        }
-    }, 1500);
+  let imdex = 0;
+  const txtJob = 'IT Specialist';
+  const speed = 100;
+
+  setTimeout(function writeJob() {
+    if (imdex < txtJob.length) {
+      document.getElementById("job").innerHTML += txtJob.charAt(imdex);
+      imdex++;
+      setTimeout(writeJob, speed);
+    }
+  }, 1500);
+
+  const COUNTER_KEY = 'visitorCounted';
+  const visitorCountEl = document.getElementById('visitor-count');
+
+  if (!sessionStorage.getItem(COUNTER_KEY)) {
+    fetch('https://hdqdi664jd.execute-api.us-east-1.amazonaws.com/visitor', {
+      method: 'GET',
+    })
+      .then(response => response.text())
+      .then(currentViews => {
+        sessionStorage.setItem(COUNTER_KEY, 'true');
+        visitorCountEl.textContent = `Visitors: ${currentViews}`;
+      })
+      .catch(err => {
+        console.error('Error counting visitor:', err);
+      });
+  }
 });
