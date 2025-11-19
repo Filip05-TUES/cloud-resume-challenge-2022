e27b6cddbd834ed1c59be742c7ae5ae829024a89
Filip05-TUES <f.ermenkov@gmail.com>
2025-11-19 03:18:19 +0200

    Update main.js

--- bucket-content/website/pages/main/main.js
+++ bucket-content/website/pages/main/main.js
@@ -1,30 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let i = 0;
-  const jobText = 'IT Specialist';
-  const speed = 100;
+    let i = 0;
+    const jobText = 'IT Specialist';
+    const speed = 100;
 
-  const typeJob = () => {
-    if (i < jobText.length) {
-      document.getElementById("job").textContent += jobText.charAt(i++);
-      setTimeout(typeJob, speed);
-    }
-  };
-  setTimeout(typeJob, 1500);
+    const typeJob = () => {
+        if (i < jobText.length) {
+            document.getElementById("job").textContent += jobText.charAt(i++);
+            setTimeout(typeJob, speed);
+        }
+    };
+    setTimeout(typeJob, 1500);
 
-  const COUNTER_KEY = 'visitorCounted';
-  const VIEWS = '0';
-  const visitorCountEl = document.getElementById('visitor-count');
+    const COUNTER_KEY = 'visitorCounted';
+    const VISITOR_COUNT_KEY = 'visitorCount';
+    const visitorCountEl = document.getElementById('visitor-count');
 
-  if (!sessionStorage.getItem(COUNTER_KEY)) {
-    fetch('https://hdqdi664jd.execute-api.us-east-1.amazonaws.com/visitor')
-      .then(res => res.text())
-      .then(count => {
-        sessionStorage.setItem(COUNTER_KEY, 'true');
-        sessionStorage.setItem(VIEWS, `${count}`);
+    const displayCount = () => {
+        const count = sessionStorage.getItem(VISITOR_COUNT_KEY) || '0';
         visitorCountEl.textContent = `Visitors: ${count}`;
-      })
-      .catch(err => {
-        isitorCountEl.textContent = `Visitors: ${sessionStorage.getItem(VIEWS)}`;
-      });
-  }
+    };
+
+    if (!sessionStorage.getItem(COUNTER_KEY)) {
+        fetch('https://hdqdi664jd.execute-api.us-east-1.amazonaws.com/visitor')
+        .then(res => res.text())
+        .then(count => {
+            sessionStorage.setItem(COUNTER_KEY, 'true');
+            sessionStorage.setItem(VISITOR_COUNT_KEY, count);
+            displayCount();
+        })
+        .catch(err => {
+            console.error('Error fetching visitor count:', err);
+            displayCount();
+        });
+    } else {
+        displayCount();
+    }
 });
